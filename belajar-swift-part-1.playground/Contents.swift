c4811c4a5e837a8adfc53440f667fbe15edde259
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2018-04-23 01:25:01 +0700

    Beginning with part 1

---
+++ belajar-swift-part-1.playground/Contents.swift
@@ -0,0 +1,41 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+// Getting started
+var hello = "Hello, swift playground!"
+print(hello)
+
+// Operation
+2 + 2
+10 - 5
+4 * 4
+18 / 2
+10.5 / 11.5
+30 % 50
+1 << 5
+24 >> 3
+300 / 10 + 5
+((1000 / (3 * 9)) - 24) >> (10 % 5)
+300 / (10 + 5)
+
+// Math function
+sin(50 * Double.pi / 150)
+cos(150 * Double.pi / 100)
+sqrt(3.0)
+max(5, 10)
+min(-2, -4)
+max(sqrt(3.0), Double.pi / 5)
+
+// Data naming
+let number: Int = 10
+let dec: Double = 5.5
+
+var total: Int = 80
+total = 50
+total += 1
+total -= 3
+
+var people: Int = 10
+people *= 4
+people /= 2
