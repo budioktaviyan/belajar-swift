da3a0b1f646a34f593857c99fc731b867fa8f49c
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2018-04-24 15:39:57 +0700

    Add learning part 2

---
+++ belajar-swift-part2.playground/Contents.swift
@@ -0,0 +1,18 @@
+import Foundation
+
+// Getting started
+var integer: Int = 100
+var decimal: Double = 12.5
+integer = Int(decimal)
+
+let hourlyLate: Double = 19.5
+let hoursWorked: Int = 10
+let totalCost: Double = hourlyLate * Double(hoursWorked)
+
+let myInteger: Int = 42
+let double: Double = 3.14159
+let typeInferredInt = 42
+let typeInfereedDouble = 3.14159
+let wantADouble = 3
+let actuallyDouble = Double(3)
+let realDouble = 3 as Double
