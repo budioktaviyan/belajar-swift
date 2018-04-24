73a84b1c58e6028127c5f97cb2e3d5d197fd45ec
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2018-04-24 13:29:57 +0700

    Add exercises for part 1

--- belajar-swift-part-1.playground/Contents.swift
+++ belajar-swift-part-1.playground/Contents.swift
@@ -1,6 +1,4 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
+import Foundation
 
 // Getting started
 var hello = "Hello, swift playground!"
@@ -39,3 +37,69 @@ total -= 3
 var people: Int = 10
 people *= 4
 people /= 2
+
+// Exercise
+let myAge: Int = 30
+let averageAge: Double = 30.0 / 30
+let testNumber: Int = 3
+let evenOdd: Int = testNumber % 2
+var myNumber: Int = 0
+myNumber += 1
+myNumber += 10 * 10 >> 3
+
+/* Challenge */
+let exercises: Int = 11
+var exercisesSolved: Int = 0
+exercisesSolved += 1
+
+var age: Int = 16
+print(age)
+age = 30
+print(age)
+exercisesSolved += 1
+
+let a: Int = 46
+let b: Int = 10
+let answer1: Int = (a * 100) + b
+let answer2: Int = (a * 100) + (b * 100)
+let answer3: Int = (a * 100) + (b / 10)
+exercisesSolved += 1
+
+(5 * 3) - 4 / (2 * 2)
+exercisesSolved -= 1
+
+let constA: Double = 1.1
+let constB: Double = 2.2
+let average: Double = constA / constB
+exercisesSolved -= 1
+
+let fahrenheit: Double = 121.0
+let celcius: Double = (fahrenheit - 32) / 1.8
+exercisesSolved += 1
+
+let position: Int = 54
+let row: Int = position / 8
+let column: Int = position % 8
+exercisesSolved += 1
+
+let dividend: Double = 5.5
+let divisor: Double = 15.5
+let quotient: Int = Int(dividend / divisor)
+let remainder: Double = dividend - divisor * Double(quotient)
+exercisesSolved += 1
+
+let degrees = 360.0
+let radians = (degrees / 180) * Double.pi
+exercisesSolved += 1
+
+let x1 = 1.0
+let y1 = 1.0
+let x2 = 3.0
+let y2 = 3.0
+let dx = x2 - x1
+let dy = y2 - y1
+let distance = sqrt(dx * dx + dy * dy)
+exercisesSolved += 1
+
+exercisesSolved += 1
+print(Double(exercisesSolved) / Double(exercises))
