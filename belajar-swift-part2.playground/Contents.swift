9f0a790d4c008cb49fab0d9ea3305db8c2325611
Budi Oktaviyan Suryanto <budioktaviyans@gmail.com>
2018-04-24 16:13:16 +0700

    Add exercises for part 2

--- belajar-swift-part2.playground/Contents.swift
+++ belajar-swift-part2.playground/Contents.swift
@@ -16,3 +16,31 @@ let typeInfereedDouble = 3.14159
 let wantADouble = 3
 let actuallyDouble = Double(3)
 let realDouble = 3 as Double
+
+// Exercises
+let age1 = 42
+let age2 = 21
+let avg1 = Double(age1 + age2) / 2
+
+let characterA: Character = "a"
+let characterHeart: Character = "\u{1F496}"
+let stringDog: String = "dog"
+let aDog = "dog"
+
+var message = "Hello, my name is "
+let name = "Budi"
+message += name
+
+let exclamationMark: Character = "!"
+message += String(exclamationMark)
+
+let myName = "Budi"
+let myMessage = "Hello my name is \(myName)!"
+let oneThird = 1.0 / 3.0
+let oneThirdLongString = "One third is \(oneThird) as a decimal."
+
+// More exercises
+let firstName = "Budi Oktaviyan"
+let lastName = "Suryanto"
+let fullName = "\(firstName) \(lastName)"
+let myDetails = "Hello, my name is \(fullName)"
